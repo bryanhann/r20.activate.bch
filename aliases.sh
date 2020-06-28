1f74df4777fc134951c524126a766080c60be5ca
Bryan Hann <nobody@nowhere>
2020-06-28 14:04:48 +0800

    Add zjot alias

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 alias vlc=/Applications/VLC.app/Contents/MacOS/VLC
-alias .z='r20_zjot new'
+alias .z='zjot new'
 alias ls='ls -F'
 alias xx='chmod +x'
 alias emacs='/usr/local/Cellar/emacs/26.3/bin/emacs'
